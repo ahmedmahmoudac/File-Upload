295dcad0636cbe644df7f7e9d56f3059b9497d66
ahmedmahmoudac <76945102+ahmedmahmoudac@users.noreply.github.com>
2023-08-23 01:21:42 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,7 +5,6 @@ export const firebaseConfig = {
     storageBucket: "file-upload-by-medoz-pro.appspot.com",
     messagingSenderId: "575667669315",
     appId: "1:575667669315:web:b5ce5f7e7b607280dd87f6",
-
   };
 
 
