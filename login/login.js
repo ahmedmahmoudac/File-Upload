122afcc7de823b7ec0fbde9446b607967699fed3
ahmedmahmoudac <76945102+ahmedmahmoudac@users.noreply.github.com>
2023-08-23 01:38:25 +0300

    Update login.js

--- login/login.js
+++ login/login.js
@@ -75,77 +75,86 @@ document.querySelector(".btn-sign-in").addEventListener("click",async()=>{
 /////* end firebase */////
 
 
+document.querySelector(".btn-sign-up").addEventListener("click", () => {
+    Swal.fire({
+        title: 'Account Creation Disabled 😥',
+        text: 'Sorry, account creation is currently disabled, if you need account subscribe medoz pro group you will get account and medoz pro vip group forever for only 100$ fees pay one time only. 😥',
+        icon: 'warning',
+        confirmButtonText: 'Subscribe Now 100$',
+        showCancelButton: true,
+        cancelButtonText: 'Cancel'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.open('https://t.me/ahmedmahmoudac', '_blank');
+        }
+    });
+});
 
 
 
+// /* start create account */
 
-/* start create account */
+// document.querySelector(".btn-sign-up").addEventListener("click",async()=>{
+//     let username = document.querySelector(".username-up").value
+//     let password = document.querySelector(".password-up").value
+//     let password2 = document.querySelector(".password-up-2").value
+//     let name = username
 
-document.querySelector(".btn-sign-up").addEventListener("click",async()=>{
-    let username = document.querySelector(".username-up").value
-    let password = document.querySelector(".password-up").value
-    let password2 = document.querySelector(".password-up-2").value
-    let name = username
+//     if(username!=""&&password!=""&&password2!=""&&password==password2)
+//     {
+//         Swal.fire({
+//             title: 'Please Wait!',
+//             didOpen: () => {Swal.showLoading()}
+//         });
 
+//       function idGenerator() {
+//         var S4 = function() {
+//             return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+//         };
+//         return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+//       };
 
-    if(username!=""&&password!=""&&password2!=""&&password==password2)
-    {
+//       let id = idGenerator();
 
-        Swal.fire({
-            title: 'Please Wait!',
-            didOpen: () => {Swal.showLoading()}
-        });
+//       let q = query(collection(db, "accounts"), where("username", "==", `${username}`));
 
-      function idGenerator() {
-        var S4 = function() {
-            return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-        };
-        return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
-      };
-
-      let id = idGenerator();
-
-      let q = query(collection(db, "accounts"), where("username", "==", `${username}`));
-
-      const querySnapshot = await getDocs(q);
-      if(querySnapshot.docs.length==0){
-        setDoc(doc(db,"accounts",id),{
-            id: id,
-            name: name,
-            username: username,
-            password: password,
-            date: Date.now(),
-        }).then(e=>{
-            Swal.fire(
-            'تم انشاء الحساب',
-            'يمكنك الان تسجيل الدخول',
-            'success'
-          )
-        });
+//       const querySnapshot = await getDocs(q);
+//       if(querySnapshot.docs.length==0){
+//         setDoc(doc(db,"accounts",id),{
+//             id: id,
+//             name: name,
+//             username: username,
+//             password: password,
+//             date: Date.now(),
+//         }).then(e=>{
+//             Swal.fire(
+//             'تم انشاء الحساب',
+//             'يمكنك الان تسجيل الدخول',
+//             'success'
+//           )
+//         });
       
-        document.querySelector(".username-up").value=""
-        document.querySelector(".password-up").value=""
-        document.querySelector(".password-up-2").value=""
+//         document.querySelector(".username-up").value=""
+//         document.querySelector(".password-up").value=""
+//         document.querySelector(".password-up-2").value=""
     
-        document.querySelector("#tab-1").click()
-      } else {
-        Swal.fire(
-            'الاسم موجود بالفعل',
-            'برجاء اختيار اسم اخر',
-            'error'
-        )
-      }
+//         document.querySelector("#tab-1").click()
+//       } else {
+//         Swal.fire(
+//             'الاسم موجود بالفعل',
+//             'برجاء اختيار اسم اخر',
+//             'error'
+//         )
+//       }
+//     } else if(username!=""&&password!=password2) {
+//         Swal.fire("","The Two Password Should be the Same","error")
+//     } else {
+//         Swal.fire("","Enter Username,Password","error")
+//     }
+// })
 
-      
+// /* end create account */
 
-    } else if(username!=""&&password!=password2) {
-        Swal.fire("","The Two Password Should be the Same","error")
-    } else {
-        Swal.fire("","Enter Username,Password","error")
-    }
-})
-
-/* end create account */
 
 
 
